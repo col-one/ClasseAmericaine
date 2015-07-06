9698fe0d2ab1386a7cf95b915898d8eeb8d3672b
col-one <colin-one@hotmail.com>
2015-07-06 20:11:04 +0200

    first
    first

---
+++ Assets/Scripts/ValideReplique.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Text;
+
+public class ValideReplique : MonoBehaviour {
+
+	public string bonneReponse;
+	private string reponse;
+
+	public void ValidateRep () 
+	{
+		reponse = GameObject.Find("Reponse").transform.FindChild("Text").GetComponent<Text>().text;
+		Debug.Log(reponse);
+		reponse = RemoveDiacriticsAndCap(reponse);
+		Debug.Log(reponse);
+	}
+
+	
+	
+	public string RemoveDiacriticsAndCap(string input)
+	{
+		string stFormD = input.Normalize(NormalizationForm.FormD);
+		int len = stFormD.Length;
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < len; i++)
+		{
+			System.Globalization.UnicodeCategory uc = System.Globalization.CharUnicodeInfo.GetUnicodeCategory(stFormD[i]);
+			if (uc != System.Globalization.UnicodeCategory.NonSpacingMark)
+			{
+				sb.Append(stFormD[i]);
+			}
+		}
+		return (sb.ToString().Normalize(NormalizationForm.FormC)).ToLower();
+	}
+	
+
+	
+	
+}
