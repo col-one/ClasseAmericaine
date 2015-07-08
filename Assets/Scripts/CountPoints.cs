2321a9d090e3dbafbe0beb8b74354bf1660c84b8
col-one <colin-one@hotmail.com>
2015-07-08 09:40:25 +0200

    ++
    ++

---
+++ Assets/Scripts/CountPoints.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CountPoints : MonoBehaviour {
+
+	public int count = 0;
+	private Text compteur;
+
+	// Use this for initialization
+	void Awake() 
+	{
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	void Start()
+	{
+		compteur = GameObject.Find("Compteur").GetComponent<Text>();
+	}
+	void Update()
+	{
+		compteur.text = (string)count.ToString();
+	}
+}
--- Assets/Scripts/ValideReplique.cs
+++ Assets/Scripts/ValideReplique.cs
@@ -5,26 +5,95 @@ using System.Text;
 
 public class ValideReplique : MonoBehaviour {
 
+	public bool isPropo;
+	private CountPoints countScript;
 	private GameObject audioSource;
 	public string bonneReponse;
 	private string reponse;
 	private GameObject panelAnswer;
+	private GameObject reponseText;
+	private GameObject imageSon;
+	private GameObject votreReponse;
+	private bool isPlay = false;
+	private bool isAnswer = false;
+	private string repToDisplay;
+	private int pointToAdd;
+	private Color colorToPut;
+
+	public void Update()
+	{
+		DisplayAnswer(repToDisplay, pointToAdd, colorToPut);
+	}
 
 	public void Awake()
 	{
+		countScript = GameObject.Find("CountObject").GetComponent<CountPoints>();
 		audioSource = GameObject.Find("Audio Source");
 		panelAnswer = GameObject.Find("PanelReponse");
+		reponseText = GameObject.Find("Reponse");
+		imageSon = GameObject.Find("ImageSon");
+		votreReponse = GameObject.Find("VotreReponse");
 		panelAnswer.SetActive(false);
 	}
 
 	public void ValidateRep () 
 	{
-		reponse = GameObject.Find("Reponse").transform.FindChild("Text").GetComponent<Text>().text;
+		reponse = GameObject.Find("ReponseChamps").transform.FindChild("Text").GetComponent<Text>().text;
 		reponse = RemoveDiacriticsAndCap(reponse);
+
 		if (reponse == bonneReponse)
 		{
+			votreReponse.GetComponent<Text>().text = reponse;
 			panelAnswer.SetActive(true);
-			audioSource.GetComponent<AudioSource>().PlayDelayed(3);
+			imageSon.SetActive(true);
+			audioSource.GetComponent<AudioSource>().PlayDelayed(2);
+			repToDisplay = bonneReponse;
+			colorToPut = Color.green;
+			if(isPropo)
+			{
+				pointToAdd = 2;
+			}
+			else
+			{
+				pointToAdd = 3;
+			}
+			isAnswer = true;
+		}
+
+		else
+		{
+			votreReponse.GetComponent<Text>().text = reponse;
+			panelAnswer.SetActive(true);
+			imageSon.SetActive(true);
+			audioSource.GetComponent<AudioSource>().PlayDelayed(2);
+			repToDisplay = bonneReponse;
+			colorToPut = Color.red;
+			pointToAdd = -1;
+			isAnswer = true;
+
+		}
+	}
+
+	public void StopAllSound()
+	{
+
+	}
+
+	public void ReListen()
+	{
+		audioSource.GetComponent<AudioSource>().PlayDelayed(1);
+	}
+
+	public void DisplayAnswer(string repToDiplay, int pointToAdd, Color colorToPut)
+	{
+		isPlay = audioSource.GetComponent<AudioSource>().isPlaying;
+		if(!isPlay && isAnswer)
+		{
+			imageSon.SetActive(false);
+			reponseText.GetComponent<Text>().text = repToDiplay;
+			countScript.count += pointToAdd;
+			votreReponse.GetComponent<Text>().color = colorToPut;
+			isAnswer = false;
 		}
 	}
 
