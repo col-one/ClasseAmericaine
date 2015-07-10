93e7df905970ef284fe1b7ac4de5a2e4c21b46f8
col-one <colin-one@hotmail.com>
2015-07-11 00:05:49 +0200

    fix input bug

---
+++ Assets/Scripts/FixBugTextField.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class FixBugTextField : MonoBehaviour {
+	private string backUpString = "";
+	private InputField input;
+	
+	void Start (){
+		input = GetComponent<InputField> ();
+		input.onValueChange.AddListener (OnChangeValue);
+	}
+	
+	void OnDestroy(){
+		input.onValueChange.RemoveAllListeners ();
+	}
+	
+	void OnChangeValue (string value){
+		if (string.IsNullOrEmpty (value) && backUpString.Length > 1) {
+			input.text = backUpString;
+			return;
+		}
+		
+		backUpString = value;
+	}
+}
